c1b33bb40abc72540b19133d2a8b7a7a50ab890e
JenkinsTheme <12alexman@gmail.com>
2020-05-23 20:23:07 +0300

    added Image transition

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,72 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
+import $ from 'jquery';
+import delay from './utils/dom';
 import 'babel-polyfill'
+import  '../bg-1.jpg'
+import  '../bg-4.jpg'
 
-var 
+const 
     preloader = document.getElementById('preloader'),
     preloaderInner = document.getElementById('preloader-inner');
 
-function delay(n) {
-    n = n || 2000;
-    return new Promise(done => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
-
 function PJAXPrepare() {
-
-    var 
-        tl = gsap.timeline();
-
-    tl.set(preloader, {
+    gsap.timeline().set(preloader, {
         opacity: 1, 
         visibility: 'visible',
     });
-
-    tl.set(preloaderInner, {
+    gsap.timeline().set(preloaderInner, {
         scaleX: 0,
         transformOrigin: 'left center'
     })
-
 }
 
 function PJAXinitPage(){
-    console.log('init')
+   console.log('you could init there some shit');
 }
 
-var PJAXTransitionBlock = {
+function PJAXCalcPositionTop(){
+    return $('.shit').get(0).getBoundingClientRect().top;
+}
 
+function CloneImage(data, selector){
+    let 
+        target = $(data.trigger),
+        img = selector ? selector : target.find('img[src]'),
+        body = $('body'),
+        clone = img.clone(),
+        imgPosition = img.get(0).getBoundingClientRect();
+
+    clone.appendTo(body);
+    clone.addClass('is-clone');
+    gsap.timeline()
+        .set(clone, {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            x: imgPosition.left,
+            y: imgPosition.top,
+			zIndex: 300
+        })
+}
+
+const PJAXTransitionBlock = {
+    name: 'simple',
+	custom: ({
+		current,
+		next,
+		trigger
+	}) => {
+        return $(trigger).data('pjax-link') == 'simple';
+	},
     async leave(data) {
     
         const done = this.async();
 
         await PJAXPrepare();
 
-        var 
-        tl = gsap.timeline();
 
- 
-        tl.to(preloaderInner, {
+        gsap.timeline().to(preloaderInner, {
             scaleX: 1,
             transformOrigin: 'left center',
             ease: 'Expo.easeInOut'
@@ -60,11 +78,9 @@ var PJAXTransitionBlock = {
 
     },
 
-    async enter(data) {
-        var 
-            tl = gsap.timeline();
+    enter() {
 
-        tl.to(preloaderInner, {
+        gsap.timeline().to(preloaderInner, {
             scaleX: 0,
             transformOrigin: 'right center',
             ease: 'Expo.easeInOut'
@@ -72,19 +88,91 @@ var PJAXTransitionBlock = {
 
     },
 
-    async afterEnter(data) {
+    afterEnter() {
         return PJAXinitPage();
     },
 
 }
 
-var PJAX = function() {
+const PJAXTransitionImage = {
+    name: 'image',
+	custom: ({
+		current,
+		next,
+		trigger
+	}) => {
+        return $(trigger).data('pjax-link') == 'image';
+    },
+
+    async leave(data) {
+    
+        const done = this.async();
+
+        await CloneImage(data);
+
+        console.log('shit anim')
+
+        let clone = $('.is-clone');
+
+        gsap.timeline()
+            .to(clone, {
+                x: 0,
+                y: 0,
+                width: '100%',
+                height: '100%',
+                duration: 0.8,
+                ease: 'Expo.easeInOut',
+            });
+
+
+        await delay(1000);
+
+        done();
+
+    },
+
+    async enter(data) {
+        return PJAXinitPage();
+    },
+
+    async afterEnter(data) {
+        await delay(500);
+        PJAXAnimateNewImg(data);
+    },
+
+}
+
+function PJAXAnimateNewImg(data){
+    let 
+        nextContainer = $(data.next.container),
+        clone = $('.is-clone'),
+        newImg = nextContainer.find('.shit'),
+        newImagePosition = newImg.get(0).getBoundingClientRect();
+
+    gsap.timeline()  
+        .to(clone, {
+            x: newImagePosition.left,
+            y: PJAXCalcPositionTop,
+            width: newImagePosition.width,
+            height: newImagePosition.height,
+            duration: 0.8,
+            ease: 'Expo.easeInOut',
+        })
+        .add(
+            function(){
+                clone.remove();
+            }
+        );
+}
+
+const PJAX = function() {
     barba.init({
 
         sync: true,
     
         transitions: [
-            PJAXTransitionBlock
+            PJAXTransitionBlock,
+            PJAXTransitionImage
         ]
     });
 }
---
+++ src/js/utils/dom.js
@@ -0,0 +1,8 @@
+export default function delay(n) {
+    n = n || 2000;
+    return new Promise(done => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
