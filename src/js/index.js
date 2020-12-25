8ca4059fe224d274c902ccc74c2520fe110b4718
Sbr <12alexman@gmail.com>
2020-12-25 11:09:44 +0200

    refactor: remove useless functions, rename variables

--- src/js/index.js
+++ src/js/index.js
@@ -1,180 +1,110 @@
-import barba from '@barba/core';
-import gsap from 'gsap';
-import $ from 'jquery';
-import delay from './utils/dom';
 import 'babel-polyfill'
-import  '../images/bg-1.jpg'
-import  '../images/bg-4.jpg'
+import $ from 'jquery'
+import gsap from 'gsap'
+import barba from '@barba/core'
+import barbaPrefetch from '@barba/prefetch'
 
-const 
+import './images.js'
+
+const
     preloader = document.getElementById('preloader'),
-    preloaderInner = document.getElementById('preloader-inner');
+    preloaderInner = document.getElementById('preloader-inner'),
+    gsapTl = gsap.timeline()
 
-function PJAXPrepare() {
-    gsap.timeline().set(preloader, {
-        opacity: 1, 
-        visibility: 'visible',
-    });
-    gsap.timeline().set(preloaderInner, {
-        scaleX: 0,
-        transformOrigin: 'left center'
+function cloneImage(data, selector) {
+    const
+        img = selector ? selector : $(data.trigger).find('img[src]'),
+        $clone = img.clone(),
+        imgPosition = img.get(0).getBoundingClientRect()
+
+    $clone.appendTo($('body'))
+    $clone.addClass('is-clone')
+    gsapTl.set($clone, {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        x: imgPosition.left,
+        y: imgPosition.top,
+        zIndex: 300
     })
 }
 
-function PJAXinitPage(){
-   console.log('you could init there some shit');
+function pjaxAnimateNewImg(data) {
+    const
+        $clone = $('.is-clone'),
+        newImagePosition = $(data.next.container).find('.shit').get(0).getBoundingClientRect()
+
+    gsapTl.to($clone, {
+        x: newImagePosition.left,
+        y: newImagePosition.top,
+        width: newImagePosition.width,
+        height: newImagePosition.height,
+        duration: 0.8,
+        ease: 'Expo.easeInOut'
+    }).add(
+        () => {
+            $clone.remove()
+        }
+    )
 }
 
-function PJAXCalcPositionTop(){
-    return $('.shit').get(0).getBoundingClientRect().top;
-}
-
-function CloneImage(data, selector){
-    let 
-        target = $(data.trigger),
-        img = selector ? selector : target.find('img[src]'),
-        body = $('body'),
-        clone = img.clone(),
-        imgPosition = img.get(0).getBoundingClientRect();
-
-    clone.appendTo(body);
-    clone.addClass('is-clone');
-    gsap.timeline()
-        .set(clone, {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            x: imgPosition.left,
-            y: imgPosition.top,
-			zIndex: 300
-        })
-}
-
-const PJAXTransitionBlock = {
+const pjaxTransitionBlock = {
     name: 'simple',
-	custom: ({
-		current,
-		next,
-		trigger
-	}) => {
-        return $(trigger).data('pjax-link') == 'simple';
-	},
-    async leave(data) {
-    
-        const done = this.async();
+    custom: ({trigger}) => $(trigger).data('pjax-link') === 'simple',
+    leave() {
+        gsapTl.set(preloader, {
+            opacity: 1,
+            visibility: 'visible'
+        })
 
-        await PJAXPrepare();
+        gsapTl.set(preloaderInner, {
+            scaleX: 0,
+            transformOrigin: 'left center'
+        })
 
-
-        gsap.timeline().to(preloaderInner, {
+        gsapTl.to(preloaderInner, {
             scaleX: 1,
             transformOrigin: 'left center',
+            duration: 0.8,
             ease: 'Expo.easeInOut'
         })
-
-        await delay(1000);
-
-        done();
-
     },
-
     enter() {
 
-        gsap.timeline().to(preloaderInner, {
+        gsapTl.to(preloaderInner, {
             scaleX: 0,
             transformOrigin: 'right center',
+            duration: 0.8,
             ease: 'Expo.easeInOut'
         })
 
-    },
-
-    afterEnter() {
-        return PJAXinitPage();
-    },
-
+    }
 }
 
-const PJAXTransitionImage = {
+const pjaxTransitionImage = {
     name: 'image',
-	custom: ({
-		current,
-		next,
-		trigger
-	}) => {
-        return $(trigger).data('pjax-link') == 'image';
-    },
-
-    async leave(data) {
-    
-        const done = this.async();
-
-        await CloneImage(data);
-
-        console.log('shit anim')
-
-        let clone = $('.is-clone');
-
-        gsap.timeline()
-            .to(clone, {
-                x: 0,
-                y: 0,
-                width: '100%',
-                height: '100%',
-                duration: 0.8,
-                ease: 'Expo.easeInOut',
-            });
-
-
-        await delay(1000);
-
-        done();
-
-    },
-
-    async enter(data) {
-        return PJAXinitPage();
-    },
-
-    async afterEnter(data) {
-        await delay(500);
-        PJAXAnimateNewImg(data);
-    },
-
-}
-
-function PJAXAnimateNewImg(data){
-    let 
-        nextContainer = $(data.next.container),
-        clone = $('.is-clone'),
-        newImg = nextContainer.find('.shit'),
-        newImagePosition = newImg.get(0).getBoundingClientRect();
-
-    gsap.timeline()  
-        .to(clone, {
-            x: newImagePosition.left,
-            y: PJAXCalcPositionTop,
-            width: newImagePosition.width,
-            height: newImagePosition.height,
+    custom: ({trigger}) => $(trigger).data('pjax-link') === 'image',
+    leave(data) {
+        cloneImage(data)
+        gsapTl.to('.is-clone', {
+            x: 0,
+            y: 0,
+            width: '100%',
+            height: '100%',
             duration: 0.8,
-            ease: 'Expo.easeInOut',
+            ease: 'Expo.easeInOut'
         })
-        .add(
-            function(){
-                clone.remove();
-            }
-        );
+    },
+    after(data) {
+        return pjaxAnimateNewImg(data)
+    }
 }
 
-const PJAX = function() {
-    barba.init({
+barba.use(barbaPrefetch)
 
-        sync: true,
-    
-        transitions: [
-            PJAXTransitionBlock,
-            PJAXTransitionImage
-        ]
-    });
-}
-
-new PJAX();
+barba.init({
+    transitions: [
+        pjaxTransitionBlock,
+        pjaxTransitionImage
+    ]
+})
